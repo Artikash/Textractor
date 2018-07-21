314c17b4351c7676b92ec627f729fa0bc4157748
Akash Mozumdar <akashmozumdar@gmail.com>
2018-07-20 22:10:03 -0400

    fix? hostinterop

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HostInterop;
 
 namespace GUI
 {
@@ -14,6 +15,7 @@ namespace GUI
         [STAThread]
         static void Main()
         {
+            if (!Host.Start()) return;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
