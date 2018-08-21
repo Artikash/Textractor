47ea350eaa7d7ef0cf9397414c0797df7c34b478
Akash Mozumdar <akashmozumdar@gmail.com>
2018-08-21 12:41:49 -0400

    translate everything

--- extensions/googletranslate.cpp
+++ extensions/googletranslate.cpp
@@ -49,7 +49,7 @@ extern "C"
 		wchar_t translation[10000] = {};
 		wchar_t* message = error;
 
-		if (wcslen(sentence) > 2000 || GetProperty("text number", miscInfo) == 0 || GetProperty("current select", miscInfo) == 0) return sentence;
+		if (wcslen(sentence) > 2000 || GetProperty("text number", miscInfo) == 0) return sentence;
 
 		if (internet)
 		{
