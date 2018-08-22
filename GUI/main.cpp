4c5814d1a7f02d7830d05d776859f12804c27c24
Akash Mozumdar <akashmozumdar@gmail.com>
2018-08-22 12:44:16 -0400

    no one saw that

--- GUI/main.cpp
+++ GUI/main.cpp
@@ -3,18 +3,9 @@
 
 int main(int argc, char *argv[])
 {
-	_CrtMemState memStates[100] = {};
-	_CrtMemCheckpoint(memStates);
-	{
-		QApplication a(argc, argv);
-		MainWindow w;
-		w.show();
+	QApplication a(argc, argv);
+	MainWindow w;
+	w.show();
 
-
-		a.exec();
-	}
-	_CrtMemCheckpoint(memStates + 1);
-	if (_CrtMemDifference(memStates + 2, memStates + 0, memStates + 1))
-		_CrtMemDumpStatistics(memStates + 2);
-	return 0;
+	return a.exec;
 }
