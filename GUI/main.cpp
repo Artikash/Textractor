072840e3078a296401ac4ec278addc574eb1d518
Akash Mozumdar <akashmozumdar@gmail.com>
2018-09-01 14:57:25 -0400

    add exception handler

--- GUI/main.cpp
+++ GUI/main.cpp
@@ -1,8 +1,19 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+LONG WINAPI ExceptionHandler(EXCEPTION_POINTERS* exception)
+{
+	std::wstring errorMsg(L"");
+	errorMsg += L"Error code: " + std::to_wstring(exception->ExceptionRecord->ExceptionCode);
+	errorMsg += L"\r\nError address: " + std::to_wstring((DWORD)exception->ExceptionRecord->ExceptionAddress);
+	errorMsg += L"\r\nAdditional info: " + std::to_wstring(exception->ExceptionRecord->ExceptionInformation[1]);
+	MessageBoxW(NULL, errorMsg.c_str(), L"NextHooker ERROR", MB_ICONERROR);
+	return EXCEPTION_CONTINUE_SEARCH;
+}
+
 int main(int argc, char *argv[])
 {
+	AddVectoredExceptionHandler(1, ExceptionHandler);
 	QApplication a(argc, argv);
 	MainWindow w;
 	w.show();
