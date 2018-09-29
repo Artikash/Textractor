baa7923be28502af286237518046f2f913f534a0
Akash Mozumdar <akashmozumdar@gmail.com>
2018-09-29 13:10:07 -0400

    fix oboe

--- GUI/host/host.h
+++ GUI/host/host.h
@@ -31,7 +31,7 @@ namespace Host
 
 inline std::wstring StringToWideString(const std::string& text, UINT encoding)
 {
-	std::wstring ret(text.size(), 0);
+	std::wstring ret(text.size() + 1, 0);
 	ret.resize(MultiByteToWideChar(encoding, 0, text.c_str(), -1, ret.data(), ret.capacity()));
 	return ret;
 }
