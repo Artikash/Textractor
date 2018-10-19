16666ac6999af81b00eed65de832a6189ce05451
Akash Mozumdar <akashmozumdar@gmail.com>
2018-10-18 23:42:53 -0400

    add some tests

---
+++ GUI/tests.cpp
@@ -0,0 +1,14 @@
+#include "extensions.h"
+#include "misc.h"
+
+static int TESTS = []
+{
+	assert(ParseCode("/HQ-c*C:C*1C@4AA:gdi.dll:GetTextOutA"));
+	assert(ParseCode("/HB4@0"));
+	assert(ParseCode("/RS*10@44"));
+	assert(!ParseCode("HQ@4"));
+	assert(!ParseCode("/RW@44"));
+	assert(!ParseCode("/HWG@33"));
+
+	return 0;
+}();
