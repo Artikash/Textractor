74f2bc1c4d060db33db4c5c42e6d9fd9c0c0c0e7
Akash Mozumdar <akashmozumdar@gmail.com>
2018-10-19 15:55:48 -0400

    build system improvements

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,4 @@
-cd Builds/x86-Release/Build; 
+cd Builds/RelWithDebInfo/x86; 
 Compress-Archive -Force -Path "Textractor.exe","styles","platforms","Qt5Core.dll","Qt5Gui.dll","Qt5Widgets.dll","vnrhook.dll","256_Remove Repetition.dll","512_Copy to Clipboard.dll","1024_Bing Translate.dll","2048_Extra Newlines.dll" -DestinationPath Textractor;
-cd ../../x64-Release/Build;
+cd ../x64;
 Compress-Archive -Force -Path "Textractor.exe","styles","platforms","Qt5Core.dll","Qt5Gui.dll","Qt5Widgets.dll","vnrhook.dll","256_Remove Repetition.dll","512_Copy to Clipboard.dll","1024_Bing Translate.dll","2048_Extra Newlines.dll" -DestinationPath Textractor;
