1d17902c642ef121882aa877016dfd8ad6d71ae3
Akash Mozumdar <akashmozumdar@gmail.com>
2018-11-15 00:06:10 -0500

    add error handling in clipboard copier

--- extensions/copyclipboard/copyclipboard.cpp
+++ extensions/copyclipboard/copyclipboard.cpp
@@ -4,12 +4,12 @@ bool ProcessSentence(std::wstring& sentence, SentenceInfo sentenceInfo)
 {
 	if (sentenceInfo["current select"] && sentenceInfo["hook address"] != -1)
 	{
+		if (!OpenClipboard(FindWindowW(NULL, L"Textractor"))) return false;
 		HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, (sentence.size() + 2) * sizeof(wchar_t));
 		memcpy(GlobalLock(hMem), sentence.c_str(), (sentence.size() + 2) * sizeof(wchar_t));
-		GlobalUnlock(hMem);
-		OpenClipboard(0);
 		EmptyClipboard();
 		SetClipboardData(CF_UNICODETEXT, hMem);
+		GlobalUnlock(hMem);
 		CloseClipboard();
 	}
 	return false;
