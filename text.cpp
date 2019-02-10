6c47a2735105815e3d0720a648477bb4512f6376
Akash Mozumdar <akashmozumdar@gmail.com>
2019-02-10 14:08:08 -0500

    update text

--- text.cpp
+++ text.cpp
@@ -51,7 +51,8 @@ Tutorial video: https://www.youtube.com/watch?v=eecEOacF6mw
 Please contact me with any problems, feature requests, or questions relating to Textractor
 You can do so via the project homepage (issues section) or via email
 Source code available under GPLv3 at project homepage
-I'm currently looking for a new job: email me if you know anyone hiring US software engineers)";
+I'm currently looking for a new job: email me if you know anyone hiring US software engineers
+If you like this project, please tell everyone about it :))";
 const wchar_t* UPDATE_AVAILABLE = L"Update available: download it from https://github.com/Artikash/Textractor/releases";
 const wchar_t* ALREADY_INJECTED = L"Textractor: already injected";
 const wchar_t* ARCHITECTURE_MISMATCH = L"Textractor: architecture mismatch: try 32 bit Textractor instead";
