76d716956d76a38f27f9edc1e98013cb3df791f8
Blu3train <frtempra@gmail.com>
2021-05-15 11:07:22 +0200

    DevTools Papago Translate

--- deploy.ps1
+++ deploy.ps1
@@ -45,6 +45,7 @@ foreach ($language in @{
 			"Copy to Clipboard",
 			"DeepL Translate",
 			"DevTools DeepL Translate",
+			"DevTools Papago Translate",
 			"Extra Newlines",
 			"Extra Window",
 			"Google Translate",
