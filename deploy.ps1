a17d8d5993003630d11d8a03ce7402256e279edd
Akash Mozumdar <akashmozumdar@gmail.com>
2021-11-28 04:57:16 -0700

    bundle unicode font

--- deploy.ps1
+++ deploy.ps1
@@ -93,7 +93,7 @@ foreach ($arch in @("x86", "x64"))
 rm -Force -Recurse -Verbose "Textractor";
 mkdir -Force -Verbose "Textractor";
 copy -Force -Recurse -Verbose -Destination "Textractor" -Path @("Runtime/*", "Textractor--$version/*");
-&"C:\Program Files\7-Zip\7z" a "Textractor-$version-Zip-Version-English-Only.zip" Textractor/
+&"C:\Program Files\7-Zip\7z" a "Textractor-$version-Zip-Version-English-Only.zip" Textractor/ ../INSTALL_THIS_UNICODE_FONT.ttf
 
 cd ..;
 &"C:\Program Files (x86)\Inno Setup 6\iscc" -DVERSION="$version" installer.iss;
