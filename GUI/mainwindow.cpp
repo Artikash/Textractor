0b906ead0d4507e839371f744769c7b8020572e1
Blu3train <frtempra@gmail.com>
2023-03-31 20:17:05 +0200

    Force detach process on exit

--- GUI/mainwindow.cpp
+++ GUI/mainwindow.cpp
@@ -689,6 +689,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 
 MainWindow::~MainWindow()
 {
+	DetachProcess(); //force detach process
 	Settings().setValue(WINDOW, geometry());
 	CleanupExtensions();
 	SetErrorMode(SEM_NOGPFAULTERRORBOX);
